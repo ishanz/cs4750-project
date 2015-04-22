71c46c5ef514a327319fb9cc912bb2565bbcb41a
smr3ze <smr3ze@virginia.edu>
2015-04-21 20:30:40 -0400

    other fixes

--- Instructor.py
+++ Instructor.py
@@ -27,7 +27,7 @@ class Instructor:
         cur.execute("SELECT course_id, credits, course_name FROM teaches2 "
                     "WHERE id ='" + self.username + "';")
         course_data = cur.fetchall()
-        print course_data
+        #print course_data
         cur.close()
         conn.close()
         return course_data
@@ -39,7 +39,7 @@ class Instructor:
         cur.execute("SELECT id, first_name, last_name, course_grade FROM takes1 NATURAL JOIN takes3 "
                     "WHERE course_id ='" + cid + "';")
         student_list = cur.fetchall()
-        print student_list
+        #print student_list
         cur.close()
         conn.close()
         return student_list
@@ -50,7 +50,7 @@ class Instructor:
         cur = conn.cursor()
         cur.execute("SELECT assignment_id, file_path_assignment FROM assigns2 WHERE course_id= '" + cid + "';")
         assignment_list = cur.fetchall()
-        print assignment_list
+        #print assignment_list
         cur.close()
         conn.close()
         return assignment_list
@@ -61,7 +61,7 @@ class Instructor:
         cur = conn.cursor()
         cur.execute("SELECT id, first_name,last_name FROM assists1 NATURAL JOIN assists3 WHERE course_id= '" + cid + "';")
         ta_list = cur.fetchall()
-        print ta_list
+        #print ta_list
         cur.close()
         conn.close()
         return ta_list
@@ -72,7 +72,7 @@ class Instructor:
         cur = conn.cursor()
         cur.execute("SELECT resource_name, file_path_resource FROM resources WHERE course_id= '" + cid + "';")
         resource_list = cur.fetchall()
-        print resource_list
+        #print resource_list
         cur.close()
         conn.close()
         return resource_list
@@ -84,7 +84,7 @@ class Instructor:
         cur.execute("SELECT id, first_name,last_name, assignment_id, file_path_submission, submission_grade "
                     "FROM submits1 NATURAL JOIN submits2 NATURAL JOIN submits3 WHERE course_id= '" + cid + "';")
         submission_list = cur.fetchall()
-        print submission_list
+        #print submission_list
         cur.close()
         conn.close()
         return submission_list
